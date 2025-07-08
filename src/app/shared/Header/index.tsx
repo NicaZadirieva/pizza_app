2db36bba112107f51df9247fbf4f31ff8299c745
NicaZadirieva <no.demchenko@yandex.ru>
2025-07-08 23:21:22 +0300

    feat: header(desktop)

--- src/app/shared/Header/index.tsx
+++ src/app/shared/Header/index.tsx
@@ -1,10 +1,24 @@
+import CartIcon from './icon/cart-icon.svg';
 import Logo from './icon/pizzashop.svg';
 import styles from './index.module.css';
 
 export const Header = () => {
 	return (
 		<header className={styles.header}>
-			<Logo width={162} height={37}/>
+			<Logo className={styles.logo} width={162} height={37}/>
+			<nav className={styles.nav}>
+				<span className={styles['current-link']}>Home</span>
+				<a href="#">Menu</a>
+				<a href="#">Events</a>
+				<a href="#">About us</a>
+			</nav>
+			<div className={styles['btn-container']}>
+				{/**TODO: сделать кнопки компонентами */}
+				<button type="button" className={styles['login-button']}>Log in</button>
+				<button title="Корзина" type="button" className={styles['cart-button']}>
+					<CartIcon width={30} height={30}/>
+				</button>
+			</div>
 		</header>
 	);
 };
