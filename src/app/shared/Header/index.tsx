1fdebb7a3a2b6419d769f4914b776bcd1f131c86
NicaZadirieva <no.demchenko@yandex.ru>
2025-07-10 00:02:48 +0300

    refactor: remove useless code

--- src/app/shared/Header/index.tsx
+++ src/app/shared/Header/index.tsx
@@ -1,3 +1,5 @@
+import { Menu } from '../Menu';
+import { SimpleButton } from '../SimpleButton';
 import CartIcon from './icon/cart-icon.svg';
 import MenuToggleIcon from './icon/menu-toggle-icon.svg';
 import Logo from './icon/pizzashop.svg';
@@ -7,15 +9,9 @@ export const Header = () => {
 	return (
 		<header className={styles.header}>
 			<Logo className={styles.logo} width={162} height={37}/>
-			<nav className={styles.nav}>
-				<span className={styles['current-link']}>Home</span>
-				<a href="#">Menu</a>
-				<a href="#">Events</a>
-				<a href="#">About us</a>
-			</nav>
+			<Menu currentPage={'Home'}/>
 			<div className={styles['btn-container']}>
-				{/**TODO: сделать кнопки компонентами */}
-				<button type="button" className={styles['login-button']}>Log in</button>
+				<SimpleButton appearance="outlined">Log in</SimpleButton>
 				<button title="Корзина" type="button" className={styles['cart-button']}>
 					<CartIcon width={30} height={30}/>
 				</button>
