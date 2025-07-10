07ebb1487db4e685c7e6beb53ac5a5381df8e26c
NicaZadirieva <no.demchenko@yandex.ru>
2025-07-10 20:27:50 +0300

    refactor(Header): move html to components

--- src/app/shared/Header/index.tsx
+++ src/app/shared/Header/index.tsx
@@ -1,4 +1,6 @@
+import { IconedButton } from '../IconedButton';
 import { Menu } from '../Menu';
+import { MenuButton } from '../MenuButton';
 import { SimpleButton } from '../SimpleButton';
 import CartIcon from './icon/cart-icon.svg';
 import MenuToggleIcon from './icon/menu-toggle-icon.svg';
@@ -9,15 +11,15 @@ export const Header = () => {
 	return (
 		<header className={styles.header}>
 			<Logo className={styles.logo} width={162} height={37}/>
-			<Menu currentPage={'Home'}/>
+			<Menu className={styles.nav} currentPage={'Home'}/>
 			<div className={styles['btn-container']}>
-				<SimpleButton appearance="filled">Log in</SimpleButton>
-				<button title="Корзина" type="button" className={styles['cart-button']}>
+				<SimpleButton className={styles['login-button']} appearance="filled">Log in</SimpleButton>
+				<IconedButton title="Корзина" className={styles['cart-button']}>
 					<CartIcon width={30} height={30}/>
-				</button>
-				<button title="Открыть меню" type="button" className={styles['menu-button']}>
+				</IconedButton>
+				<MenuButton title="Открыть меню" className={styles['menu-button']}>
 					<MenuToggleIcon width={30} height={30} />
-				</button>
+				</MenuButton>
 			</div>
 		</header>
 	);
