e742f3bc95d19a284b9f48b51c6b30b99f84e857
NicaZadirieva <no.demchenko@yandex.ru>
2025-08-12 20:17:08 +0300

    feat: добавила стрелочку в блок AboutUs

--- src/app/entities/AboutUs/index.tsx
+++ src/app/entities/AboutUs/index.tsx
@@ -3,6 +3,7 @@ import { Paragraph } from '@/app/shared/Paragraph';
 import { Heading } from '@/app/widgets';
 import cn from 'classnames';
 import Image from 'next/image';
+import Arrow from './icon/arrow.svg';
 import styles from './index.module.css';
 
 
@@ -14,6 +15,9 @@ export const AboutUs = () => {
 				<Paragraph className={styles['paragraph']}>In just a couple of years, we have opened 6 outlets in different cities: Kazan, Chelyabinsk, Ufa, Samara, Izhevsk, and in the future we plan to develop the network in other major cities of Russia.</Paragraph>
 				<Image src="/about-us-pizzas.png" alt="about us" width={490} height={190}/>
 				<Paragraph className={styles['paragraph']}>The kitchen of each point is at least: 400-500 sq. m. meters, hundreds of employees, smoothly performing work in order to receive / prepare / form / deliver customer orders on time.</Paragraph>
+			</div>
+			<div className={cn(styles.column, styles.centerized)}>
+				<Arrow width={88} height={55}/>
 			</div>
 			{/**TODO: добавить декоративные элементы из макета */}
 			<div className={cn(styles.column, styles['relative-column'])}>
