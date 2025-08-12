0602853ebf13960279038d125c0cdc19b28fbe7d
NicaZadirieva <no.demchenko@yandex.ru>
2025-08-12 20:13:00 +0300

    feat: раздел AboutUs
    без декоративных элементов

---
+++ src/app/entities/AboutUs/index.tsx
@@ -0,0 +1,24 @@
+
+import { Paragraph } from '@/app/shared/Paragraph';
+import { Heading } from '@/app/widgets';
+import cn from 'classnames';
+import Image from 'next/image';
+import styles from './index.module.css';
+
+
+export const AboutUs = () => {
+	return (
+		<div className={styles['about-us']}>
+			<div className={styles.column}>
+				<Heading size='h2' className={styles.heading}>About Us</Heading>
+				<Paragraph className={styles['paragraph']}>In just a couple of years, we have opened 6 outlets in different cities: Kazan, Chelyabinsk, Ufa, Samara, Izhevsk, and in the future we plan to develop the network in other major cities of Russia.</Paragraph>
+				<Image src="/about-us-pizzas.png" alt="about us" width={490} height={190}/>
+				<Paragraph className={styles['paragraph']}>The kitchen of each point is at least: 400-500 sq. m. meters, hundreds of employees, smoothly performing work in order to receive / prepare / form / deliver customer orders on time.</Paragraph>
+			</div>
+			{/**TODO: добавить декоративные элементы из макета */}
+			<div className={cn(styles.column, styles['relative-column'])}>
+				<Image src="/about-us-main.png" alt="about us" width={567} height={541} />
+			</div>
+		</div>
+	);
+};
