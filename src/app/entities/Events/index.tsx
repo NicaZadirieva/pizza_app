58a6e534aa61e5489df37aae9fe82c55654a4a7b
NicaZadirieva <no.demchenko@yandex.ru>
2025-09-17 23:26:14 +0300

    fix: исправила стили блока events для мобилок (678px)

--- src/app/entities/Events/index.tsx
+++ src/app/entities/Events/index.tsx
@@ -8,19 +8,19 @@ export const Events = () => {
 		<div className={styles.events}>
 			{/**1 ряд */}
             
-			<EventItem className={styles['row1-item']} title={'How we cooking'} buttonUrl='' imageUrl="/how-we-cooking.png"/>
-			<EventItem className={styles['row1-item']}  title={'Our Blog'} buttonUrl='' imageUrl="/our-blog.png" />
+			<EventItem className={cn(styles['row1-item'], styles['event-item'])} title={'How we cooking'} buttonUrl='' imageUrl="/how-we-cooking.png"/>
+			<EventItem className={cn(styles['row1-item'], styles['event-item'])}  title={'Our Blog'} buttonUrl='' imageUrl="/our-blog.png" />
 			<div className={cn(styles['events-title'], styles['row1-item'])}>
 				<Heading size='h2'>Events</Heading>
 				<p>There are regular events in our pizzeria that will allow you to eat delicious food for a lower price!</p>
 			</div>
 			{/**2 ряд */}
-			<EventItem className={styles['row2-item1']} title={'Two pizza for 1 price'} buttonUrl='' imageUrl="/2-pizza-for-1-price.png" />
-			<EventItem className={styles['row2-item2']}  title={'Kitchen tour'} buttonUrl='' imageUrl="/kitchen-tour.png" />
+			<EventItem className={cn(styles['row2-item1'], styles['event-item'])} title={'Two pizza for 1 price'} buttonUrl='' imageUrl="/2-pizza-for-1-price.png" />
+			<EventItem className={cn(styles['row2-item2'], styles['event-item'])}  title={'Kitchen tour'} buttonUrl='' imageUrl="/kitchen-tour.png" />
 			{/**3 ряд */}
-			<EventItem className={styles['row3-item']}  title={'Free coffee for 3 pizza'} buttonUrl='' imageUrl="/free-coffee-for-3-pizza.png" />
-			<EventItem className={styles['row3-item']}  title={'Our Instagram'} buttonUrl='' imageUrl="/our-insta.png" />
-			<EventItem className={styles['row3-item']}  title={'Where are you choose us?'} buttonUrl='' imageUrl="/where-are-you-choose.png" />
+			<EventItem className={cn(styles['row3-item'], styles['event-item'])}  title={'Free coffee for 3 pizza'} buttonUrl='' imageUrl="/free-coffee-for-3-pizza.png" />
+			<EventItem className={cn(styles['row3-item'], styles['event-item'])}  title={'Our Instagram'} buttonUrl='' imageUrl="/our-insta.png" />
+			<EventItem className={cn(styles['row3-item'], styles['event-item'])}  title={'Where are you choose us?'} buttonUrl='' imageUrl="/where-are-you-choose.png" />
 		</div>
 	);
 };
