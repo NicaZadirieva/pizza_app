c4633eea8ef318469e3fbbf3407479812e8865e7
NicaZadirieva <no.demchenko@yandex.ru>
2025-09-17 23:42:44 +0300

    fix: исправила адаптив about-us

--- src/app/entities/AboutUs/index.tsx
+++ src/app/entities/AboutUs/index.tsx
@@ -15,7 +15,7 @@ export const AboutUs = () => {
 			<div className={styles.column}>
 				<Heading size='h2' className={styles.heading}>About Us</Heading>
 				<Paragraph className={styles['paragraph']}>In just a couple of years, we have opened 6 outlets in different cities: Kazan, Chelyabinsk, Ufa, Samara, Izhevsk, and in the future we plan to develop the network in other major cities of Russia.</Paragraph>
-				<Image src="/about-us-pizzas.png" alt="about us" width={490} height={190}/>
+				<Image className={styles['pizzas']} src="/about-us-pizzas.png" alt="about us" width={490} height={190}/>
 				<Paragraph className={styles['paragraph']}>The kitchen of each point is at least: 400-500 sq. m. meters, hundreds of employees, smoothly performing work in order to receive / prepare / form / deliver customer orders on time.</Paragraph>
 			</div>
 			<div className={cn(styles.column, styles.centerized)}>
@@ -24,7 +24,7 @@ export const AboutUs = () => {
 			{/**TODO: добавить декоративные элементы из макета */}
 			<div className={cn(styles.column, styles['relative-column'])}>
 				<PizzaIconTop className={styles['pizza-image']} />
-				<Image src="/about-us-main.png" alt="about us" width={567} height={541} />
+				<Image className={styles['big-pizza-image']} src="/about-us-main.png" alt="about us" width={567} height={541} />
 				<CrispsIconBottom className={styles['crisps-image']} />
 			</div>
 		</div>
